6484ebe574403288ab76f889578969f00f3734c5
reinhardjs <reinhardjsilalahi@gmail.com>
2024-01-19 20:47:14 +0700

    chore(sayakaya-technical-test): init go project

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/url"
+
+	"github.com/labstack/echo"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	dbHost := viper.GetString(`database.host`)
+	dbPort := viper.GetString(`database.port`)
+	dbUser := viper.GetString(`database.user`)
+	dbPass := viper.GetString(`database.pass`)
+	dbName := viper.GetString(`database.name`)
+	connection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+	val := url.Values{}
+	val.Add("parseTime", "1")
+	val.Add("loc", "Asia/Jakarta")
+	dsn := fmt.Sprintf("%s?%s", connection, val.Encode())
+	dbConn, err := sql.Open(`mysql`, dsn)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = dbConn.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		err := dbConn.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	e := echo.New()
+
+	log.Fatal(e.Start(viper.GetString("server.address"))) //nolint
+}
