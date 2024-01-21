f546695be18bba92426ca29f5e7e214ee238f2f1
reinhardjs <reinhardjsilalahi@gmail.com>
2024-01-22 01:22:54 +0700

    feat(sayakaya-technical-test): #NOJIRA added UnmarshalJSON and MarshalJSON for user domain

--- domain/user.go
+++ domain/user.go
@@ -2,14 +2,48 @@ package domain
 
 import (
 	"context"
+	"encoding/json"
 	"time"
 )
 
 type User struct {
 	ID             int64     `json:"id" db:"id"`
-	Email          string    `json:"title" db:"email"`
-	VerifiedStatus string    `json:"content" db:"verifiedStatus"`
+	Email          string    `json:"email" db:"email"`
+	VerifiedStatus bool      `json:"verifiedStatus" db:"verifiedStatus"`
 	Birthday       time.Time `json:"birthday" db:"birthday"`
+}
+
+func (u *User) UnmarshalJSON(data []byte) error {
+	type Alias User
+	aux := &struct {
+		Birthday string `json:"birthday"`
+		*Alias
+	}{
+		Alias: (*Alias)(u),
+	}
+
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	parsedBirthday, err := time.Parse("2006-01-02", aux.Birthday)
+	if err != nil {
+		return err
+	}
+
+	u.Birthday = parsedBirthday
+	return nil
+}
+
+func (u *User) MarshalJSON() ([]byte, error) {
+	type Alias User
+	return json.Marshal(&struct {
+		*Alias
+		Birthday string `json:"birthday"`
+	}{
+		Birthday: u.Birthday.Format("2006-01-02"),
+		Alias:    (*Alias)(u),
+	})
 }
 
 type UserUsecase interface {
